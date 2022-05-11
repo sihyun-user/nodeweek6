a3d1e7deb7044cf15dd97663e3f5d0e9effb1859
chsiyu <chsiyu8652@gmail.com>
2022-05-11 17:36:22 +0800

    docs: swagger文件撰寫

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,6 +5,28 @@ const User = require('../models/usersModel');
 
 const posts = {
   async getPosts(req, res) {
+    /*
+      #swagger.tags = ['Posts - 貼文']
+      #swagger.description = '取得全部貼文 API'
+      #swagger.responses[200] = { 
+        description: '貼文資訊',
+        schema: {
+          "status": true,
+          "data": [{
+              "_id": "627b71adbd87b7922f3688c7",
+              "user": {
+                "_id": "62787493c586f2cd6a79da0e",
+                "name": "Ada",
+                "photo": "https://thumb.fakeface.rest/thumb_female_27_5a94a297efb15caf0e3d769ce1694953e8bf33e2.jpg"
+              },
+              "content": "測試 ref 關聯資料 4",
+              "image": "",
+              "likes": 0
+          }]
+        }
+      }
+    */
+
     // 貼文關鍵字搜尋與篩選
     const timeSort = req.query.timeSort == 'asc' ? 'createdAt' : '-createdAt';
     const q = req.query.q !== undefined ? {'content': new RegExp(req.query.q)} : {};
@@ -16,6 +38,14 @@ const posts = {
   },
   async createPosts(req, res) {
     try {
+      /*
+        #swagger.tags = ['Posts - 貼文']
+        #swagger.description = '取得全部貼文 API'
+        #swagger.parameters['body'] = {
+          in: 'body',
+          description: '資料格式'
+        }
+      */
       const { user, content } = req.body;
   
       const newPost = await Post.create({
@@ -29,10 +59,16 @@ const posts = {
     };
   },
   async deleteAllPosts(req, res) {
+    /**
+     * #swagger.tags = ['Posts - 貼文']
+     */
     await Post.deleteMany();
     responseHandler.handleSuccess(res, []);
   },
   async deleteOnePosts(req, res) {
+    /**
+     * #swagger.tags = ['Posts - 貼文']
+     */
     try {
       const id = req.params.id;
       const postData = await Post.find({'_id': id});
@@ -49,6 +85,9 @@ const posts = {
     }
   },
   async updatePosts(req, res) {
+    /**
+     * #swagger.tags = ['Posts - 貼文']
+     */
     try {
       const id = req.params.id;
       const { name, content } = req.body;
