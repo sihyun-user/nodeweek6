a5bf34bbdb3ca0071caa347912cdd5adf0a6bc6b
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-05-12 01:40:49 +0800

    docs: swagger文件撰寫

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,23 +7,10 @@ const posts = {
   async getPosts(req, res) {
     /*
       #swagger.tags = ['Posts - 貼文']
-      #swagger.description = '取得全部貼文 API'
+      #swagger.description = '取得貼文 API'
       #swagger.responses[200] = { 
         description: '貼文資訊',
-        schema: {
-          "status": true,
-          "data": [{
-              "_id": "627b71adbd87b7922f3688c7",
-              "user": {
-                "_id": "62787493c586f2cd6a79da0e",
-                "name": "Ada",
-                "photo": "https://thumb.fakeface.rest/thumb_female_27_5a94a297efb15caf0e3d769ce1694953e8bf33e2.jpg"
-              },
-              "content": "測試 ref 關聯資料 4",
-              "image": "",
-              "likes": 0
-          }]
-        }
+        schema: { $ref: '#/definitions/getPosts' }
       }
     */
 
@@ -40,35 +27,57 @@ const posts = {
     try {
       /*
         #swagger.tags = ['Posts - 貼文']
-        #swagger.description = '取得全部貼文 API'
+        #swagger.description = '建立貼文 API'
         #swagger.parameters['body'] = {
           in: 'body',
-          description: '資料格式'
+          type: 'object',
+          required: true,
+          description: '資料格式',
+          schema: { 
+            $user: '6277c7231f5cea212fc08c8a',
+            $content: '貼文內容',
+            image: '圖片網址',
+          }
+        }
+        #swagger.responses[200] = { 
+          description: '貼文資訊',
+          schema: { $ref: '#/definitions/createPosts' }
         }
       */
       const { user, content } = req.body;
-  
       const newPost = await Post.create({
         user: user,
         content : content
       });
-    
       responseHandler.handleSuccess(res, newPost);
     } catch (error) {
       responseHandler.handleError(res, errorMsg.POST);
     };
   },
   async deleteAllPosts(req, res) {
-    /**
-     * #swagger.tags = ['Posts - 貼文']
-     */
+    /*
+      #swagger.tags = ['Posts - 貼文']
+      #swagger.description = '刪除全部貼文 API'
+      #swagger.responses[200] = {
+        description: '貼文資訊',
+        schema: {
+          status: true,
+          data: []
+        }
+      }
+    */
     await Post.deleteMany();
     responseHandler.handleSuccess(res, []);
   },
   async deleteOnePosts(req, res) {
-    /**
-     * #swagger.tags = ['Posts - 貼文']
-     */
+    /*
+      #swagger.tags = ['Posts - 貼文']
+      #swagger.description = '刪除單則貼文 API'
+      #swagger.responses[200] = {
+        description: '貼文資訊',
+        schema: { $ref: '#/definitions/deleteOnePosts' }
+      }
+    */
     try {
       const id = req.params.id;
       const postData = await Post.find({'_id': id});
@@ -85,9 +94,24 @@ const posts = {
     }
   },
   async updatePosts(req, res) {
-    /**
-     * #swagger.tags = ['Posts - 貼文']
-     */
+    /*
+      #swagger.tags = ['Posts - 貼文']
+      #swagger.description = '更新單則貼文 API'
+      #swagger.parameters['body'] = {
+        in: 'body',
+        type: 'object',
+        required: true,
+        description: '資料格式',
+        schema: { 
+          $content: '貼文內容',
+          image: '圖片網址',
+        }
+      }
+      #swagger.responses[200] = {
+        description: '貼文資訊'
+        schema: { $ref: '#/definitions/updatePosts }
+      }
+    */
     try {
       const id = req.params.id;
       const { name, content } = req.body;
--- swagger.js
+++ swagger.js
@@ -7,6 +7,55 @@ const doc = {
   },
   host: 'localhost:3005', // (重要) 本地:localhost:3005 | heroku:xxxx
   schemes: ['http', 'https'], // swagger文件支援哪幾種模式
+  definitions: {
+    getPosts: {
+      status: true,
+      data: [{
+        _id: '627bbdb68f689a16b42ca602',
+        user: {
+          _id: '6277c7231f5cea212fc08c8a',
+          name: 'John',
+          photo: '頭貼網址'
+        },
+        content: '貼文內容',
+        image: '圖片網址',
+        likes: 0
+      }]
+    },
+    createPosts: {
+      status: true,
+      data: {
+        _id: '627bcc153ef5abfd5c800b5f',
+        user: '6277c7231f5cea212fc08c8a',
+        content: '貼文內容',
+        image: '圖片網址',
+        likes: 0,
+        createdAt: '2022-05-11T14:45:41.770Z'
+      }
+    },
+    deleteOnePosts: {
+      status: true,
+      data: [
+        {
+          _id: '627bcc153ef5abfd5c800b5f',
+          user: '6277c7231f5cea212fc08c8a',
+          content: '貼文內容',
+          image: '圖片網址',
+          likes: 0,
+        }
+      ]
+    },
+    updatePosts: {
+      status: true,
+      data: {
+        _id: '627bcc153ef5abfd5c800b5f',
+        user: '6277c7231f5cea212fc08c8a',
+        content: '貼文內容',
+        image: '圖片網址',
+        likes: 0,
+      }
+    }
+  }
 };
 
 const outputFile = './swagger-output.json'; // 輸出的文件名稱(swagger json文件)
