64fcd6e7c680c568adb66a91776d073cda907bd7
chsiyu <chsiyu8652@gmail.com>
2022-05-12 11:45:33 +0800

    feat: 設置腳本命令環境變數、切換資料庫連線

--- connections/index.js
+++ connections/index.js
@@ -3,15 +3,15 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path:'./config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//   '<password>',
-//   process.env.DATABAES_PASSWORD
-// );
+const productionDB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABAES_PASSWORD
+);
 
-// mongoose.connect(DB)
-//   .then(() => console.log('資料庫連線成功'))
-//   .catch((err) => console.log(err));
+const DB = process.env.NODE_ENV === 'production' 
+? productionDB 
+: process.env.DATABASE_LOCAL
 
-mongoose.connect('mongodb://localhost:27017/post')
+mongoose.connect(DB)
   .then(() => console.log('資料庫連線成功'))
   .catch((err) => console.log(err));
