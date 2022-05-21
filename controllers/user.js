ca3d1a0683e1922dc51b40b74f353ce5c6ec92ba
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-05-22 02:37:32 +0800

    feat: 修改 swagger 格式

--- controllers/user.js
+++ controllers/user.js
@@ -3,53 +3,20 @@ const catchAsync = require('../service/catchAsync');
 const appSuccess = require('../service/appSuccess');
 const appError = require('../service/appError');
 
-const users = {
-  getUsers: catchAsync(async(req, res, next) => {
-    /*
-      #swagger.tags = ['User - 用戶(API測試用)']
-      #swagger.description = '取得全部用戶 API'
-      #swagger.responses[200] = {
-        description: '用戶資訊',
-        schema: { $ref: '#/definitions/getUser' }
-      }
-    */
-    const data = await User.find();
-    appSuccess({res, data});
-  }),
-  createUser: catchAsync(async(req, res, next) => {
-    /*
-      #swagger.tags = ['Users - 用戶(API測試用)']
-      #swagger.description = '建立用戶 API'
-      #swagger.parameters['body'] = {
-        in: 'body',
-        type: 'object',
-        required: true,
-        description: '資料格式',
-        schema: {
-          $name: '用戶名稱',
-          $email: 'test@gmail.com',
-          photo: ''
-        }
-      }
-      #swagger.responses[200] = {
-        description: '用戶資訊',
-        schema: {
-          status: true,
-          message: '建立用戶成功'
-        }
-      }
-    */
-    const { name, email, photo } = req.body;
+exports.getUsers = catchAsync(async(req, res, next) => {
+  const data = await User.find();
+  appSuccess({res, data});
+})
 
-    if (!name | !email) return appError(apiState.DATA_MISSING, next);
-    
-    await User.create({
-      name: name,
-      email: email,
-      photo: photo
-    });
-    appSuccess({res, message:'建立用戶成功'});
-  })
-};
+exports.createUser = catchAsync(async(req, res, next) => {
+  const { name, email, photo } = req.body;
 
-module.exports = users
+  if (!name | !email) return appError(apiState.DATA_MISSING, next);
+  
+  await User.create({
+    name: name,
+    email: email,
+    photo: photo
+  });
+  appSuccess({res, message:'建立用戶成功'});
+})
--- routes/post.js
+++ routes/post.js
@@ -2,16 +2,109 @@ const express = require('express');
 const router = express.Router();
 const PostsControllers = require('../controllers/post');
 
-router.get('/posts', PostsControllers.getAllPost);
+router.get('/posts', 
+  /*
+    #swagger.tags = ['Post - 貼文']
+    #swagger.description = '取得貼文列表 API'
+    #swagger.responses[200] = { 
+      description: '貼文資訊',
+      schema: { $ref: '#/definitions/getPosts' }
+    }
+  */
+  PostsControllers.getAllPost
+);
 
-router.get('/post/:post_id', PostsControllers.getOnePost);
+router.get('/post/:post_id',
+  /*
+    #swagger.tags= ['Post - 貼文']
+    #swagger.description = '取得單筆貼文 API'
+    #swagger.responses[200] = { 
+      description: '貼文資訊',
+      schema: { $ref: '#/definitions/getOnePost' }
+    }
+  */  
+  PostsControllers.getOnePost
+);
 
-router.post('/post', PostsControllers.createPost);
+router.post('/post',
+  /*
+    #swagger.tags = ['Post - 貼文']
+    #swagger.description = '建立貼文 API'
+    #swagger.parameters['body'] = {
+      in: 'body',
+      type: 'object',
+      required: true,
+      description: '資料格式',
+      schema: { 
+        $user: '用戶ID',
+        $content: '貼文內容',
+        image: '圖片網址',
+      }
+    }
+    #swagger.responses[200] = { 
+      description: '貼文資訊',
+      schema: {
+        status: true,
+        message: '新增貼文成功'
+      }
+    }
+  */
+  PostsControllers.createPost
+);
 
-router.patch('/post/:post_id', PostsControllers.updatePost);
+router.patch('/post/:post_id',
+  /*
+    #swagger.tags = ['Post - 貼文']
+    #swagger.description = '更新此筆貼文 API'
+    #swagger.parameters['body'] = {
+      in: 'body',
+      type: 'object',
+      required: true,
+      description: '資料格式',
+      schema: { 
+        $content: '貼文內容',
+        image: '圖片網址',
+      }
+    }
+    #swagger.responses[200] = {
+      description: '貼文資訊',
+      schema: { 
+        status: true,
+        message: '編輯此筆貼文成功'
+      }
+    }
+  */
+  PostsControllers.updatePost
+);
 
-router.delete('/posts', PostsControllers.deleteAllPost);
+router.delete('/posts',
+  /*
+    #swagger.tags = ['Post - 貼文']
+    #swagger.description = '刪除全部貼文 API'
+    #swagger.responses[200] = {
+      description: '貼文資訊',
+      schema: {
+        status: true,
+        message: '刪除所有貼文成功'
+      }
+    }
+  */
+  PostsControllers.deleteAllPost
+);
 
-router.delete('/post/:post_id', PostsControllers.deleteOnePost);
+router.delete('/post/:post_id',
+  /*
+    #swagger.tags = ['Post - 貼文']
+    #swagger.description = '刪除此筆貼文 API'
+    #swagger.responses[200] = {
+      description: '貼文資訊',
+      schema: { 
+        status: true,
+        message: '刪除此筆貼文成功'
+      }
+    }
+  */
+  PostsControllers.deleteOnePost
+);
 
 module.exports = router
